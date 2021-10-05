94607b8f440782e62b7de06f7cd3649aba30a7ae
Erika Gil <roselin.gil83@gmail.com>
2021-10-04 20:20:10 -0700

    Add project files.

---
+++ ContosoPackageProject/LockPermitCancelInspections.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.Text.RegularExpressions;
+using ContosoPackagePoject;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace ContosoPackageProject
+{
+    public class LockPermitCancelInspections : PluginBase
+    {
+        protected override void ExecuteCDSPlugin(LocalPluginContext localcontext)
+        {
+            base.ExecuteCDSPlugin(localcontext);
+
+            var permitEntityRef = localcontext.PluginExecutionContext.InputParameters["Target"] as EntityReference;
+            Entity permitEntity = new Entity(permitEntityRef.LogicalName, permitEntityRef.Id);
+
+            localcontext.Trace("Updating Permit Id : " + permitEntityRef.Id);
+            permitEntity["statuscode"] = new OptionSetValue(100000000);//463270000
+
+            localcontext.OrganizationService.Update(permitEntity);
+            localcontext.Trace("Updated Permit Id " + permitEntityRef.Id);
+
+            QueryExpression qe = new QueryExpression();
+            qe.EntityName = "contoso_inspection";
+            qe.ColumnSet = new ColumnSet("statuscode");
+
+            ConditionExpression condition = new ConditionExpression();
+            condition.Operator = ConditionOperator.Equal;
+            condition.AttributeName = "contoso_permit";
+            condition.Values.Add(permitEntityRef.Id);
+
+            qe.Criteria = new FilterExpression(LogicalOperator.And);
+            qe.Criteria.Conditions.Add(condition);
+
+            localcontext.Trace("Retrieving inspections for Permit Id " + permitEntityRef.Id);
+            var inspectionsResult = localcontext.OrganizationService.RetrieveMultiple(qe);
+            localcontext.Trace("Retrievied " + inspectionsResult.TotalRecordCount + " inspection records");
+
+            int canceledInspectionsCount = 0;
+            foreach (var inspection in inspectionsResult.Entities)
+            {
+                var currentValue = inspection.GetAttributeValue<OptionSetValue>("statuscode");
+
+                if (currentValue.Value == 1 || currentValue.Value == 100000000) //463270000
+                {
+                    canceledInspectionsCount++;
+                    inspection["statuscode"] = new OptionSetValue(100000003);//463270003
+                    localcontext.Trace("Canceling inspection Id : " + inspection.Id);
+                    localcontext.OrganizationService.Update(inspection);
+                    localcontext.Trace("Canceled inspection Id : " + inspection.Id);
+                }
+
+            }
+
+            if (canceledInspectionsCount > 0)
+            {
+                localcontext.PluginExecutionContext.OutputParameters["CanceledInspectionsCount"] = canceledInspectionsCount + " Inspections were canceled";
+            }
+
+            if (localcontext.PluginExecutionContext.InputParameters.ContainsKey("Reason"))
+            {
+
+                localcontext.Trace("building a note reocord");
+                Entity note = new Entity("annotation");
+                note["subject"] = "Permit Locked";
+                note["notetext"] = "Reason for locking this permit: " + localcontext.PluginExecutionContext.InputParameters["Reason"];
+                note["objectid"] = permitEntityRef;
+                note["objecttypecode"] = permitEntityRef.LogicalName;
+
+                localcontext.Trace("Creating a note reocord");
+                var createdNoteId = localcontext.OrganizationService.Create(note);
+
+                if (createdNoteId != Guid.Empty)
+                    localcontext.Trace("Note record was created");
+
+            }//
+
+
+        }
+    }
+}
---
+++ ContosoPackageProject/PreOperationPermitCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using ContosoPackagePoject;
+
+namespace ContosoPackageProject
+{
+    public class PreOperationPermitCreate : PluginBase
+    {
+
+        protected override void ExecuteCDSPlugin(LocalPluginContext localcontext)
+        {
+            base.ExecuteCDSPlugin(localcontext);
+
+            var permitEntity = localcontext.PluginExecutionContext.InputParameters["Target"] as Entity;
+            var buildSiteRef = permitEntity["contoso_buildsite"] as EntityReference;
+
+            localcontext.Trace("Primary Entity Id: " + permitEntity.Id);
+            localcontext.Trace("Build Site Entity Id: " + buildSiteRef.Id);
+
+            string fetchString = "<fetch output-format='xml-platform' distinct='false' version='1.0' mapping='logical' aggregate='true'>" +
+                "<entity name='contoso_permit'>" +
+                "<attribute name='contoso_permitid' alias='Count' aggregate='count' />" +
+                "<filter type='and' >" +
+                "<condition attribute='contoso_buildsite' uitype='contoso_buildsite' operator='eq' value='{" + buildSiteRef.Id + "}'/>" +
+                "<condition attribute='statuscode' operator='eq' value='100000000'/>" +
+                "</filter>" +
+                "</entity>" +
+                "</fetch>";
+
+            localcontext.Trace("Calling RetrieveMultiple for locked permits");
+            var response = localcontext.OrganizationService.RetrieveMultiple(new FetchExpression(fetchString));
+            int lockedPermitCount = (int)((AliasedValue)response.Entities[0]["Count"]).Value;
+            localcontext.Trace("Locket Permit count : " + lockedPermitCount);
+
+            if (lockedPermitCount > 0)
+            {
+                throw new InvalidPluginExecutionException("Too many locked permits for build site");
+            }
+        }//
+
+    }
+}
